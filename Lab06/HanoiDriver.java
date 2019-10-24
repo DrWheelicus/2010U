65356ce6f97add3d932c97a4b155d99a6aa126ba
DrWheelicus <hayden.macintyre@ontariotechu.net>
2019-10-23 22:36:02 -0400

    Changed variable names for clarity

--- Lab06/HanoiDriver.java
+++ Lab06/HanoiDriver.java
@@ -1,17 +1,17 @@
 public class HanoiDriver {
-    static void solveHanoi(int n, int rod1, int rod2, int rod3) { 
+    static void solveHanoi(int n, int startRod, int targetRod, int rod3) { 
         if (n == 1) { 
-            System.out.println("Move disk 1 from rod " +  rod1 + " to rod " + rod2); 
+            System.out.printf("Move top ring from %d to %d.\n", startRod, targetRod);
             return; 
         }
 
-        solveHanoi(n-1, rod1, rod3, rod2); 
-        System.out.println("Move disk " + n + " from rod " +  rod1 + " to rod " + rod2); 
-        solveHanoi(n-1, rod3, rod2, rod1); 
+        solveHanoi(n-1, startRod, rod3, targetRod); 
+        System.out.printf("Move ring " + n + " from %d to %d.\n", startRod, targetRod);
+        solveHanoi(n-1, rod3, targetRod, startRod); 
     }
 
     public static void main(String[] args) {
         System.out.println("Towers of Hanoi:");
-        solveHanoi(4, 3, 2, 4);
+        solveHanoi(1, 3, 2, 4);
     }
 }
