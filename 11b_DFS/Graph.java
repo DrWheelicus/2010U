62df1789f275585effa31a82e0d0f9b4546e509a
DrWheelicus <hayden.macintyre@ontariotechu.net>
2019-12-01 16:43:03 -0500

    Updated the DFS example

--- 11b_DFS/Graph.java
+++ 11b_DFS/Graph.java
@@ -1,83 +1,85 @@
 import java.util.*;
 
-public class Graph {
-   private class Vertex {
-      private String name;
-      private List<Vertex> adjacencies;
+public class AdjacencyListGraph {
+    private class Vertex {
+        private String name;
+        private List<Vertex> adjacencies;
 
-      public Vertex(String name) {
-         this.name = name;
-         adjacencies = new ArrayList<>();
-      }
+        public Vertex(String name) {
+            this.name = name;
+            adjacencies = new ArrayList<>();
+        }
 
-      public String getName() { return this.name; }
+        public String getName() { return name; }
 
-      public void addAdjacency(Vertex other) {
-         adjacencies.add(other);
-      }
+        public void addAdjacency(Vertex other) {
+            adjacencies.add(other);
+        }
 
-      public List<Vertex> getAdjacencies() { return adjacencies; }
+        public List<Vertex> getAdjacencies() {
+            return adjacencies;
+        }
 
-      public int getDegree() {
-         return adjacencies.size();
-      }
-   }
+        public int getDegree() {
+            return adjacencies.size();
+        }
+    }
 
-   private class Edge {
-      private Vertex v1;
-      private Vertex v2;
+    private HashMap<String,Vertex> vertices;
 
-      public Edge(Vertex v1, Vertex v2) {
-         this.v1 = v1;
-         this.v2 = v2;
-      }
+    public AdjacencyListGraph() {
+        vertices = new HashMap<>();
+    }
 
-      public Vertex getVertex1() { return v1; }
-      public Vertex getVertex2() { return v2; }
-   }
+    public void addVertex(String name) {
+        vertices.put(name, new Vertex(name));
+    }
 
-   private HashMap<String, Vertex> vertices;
+    public void addEdge(String v1name, String v2name) {
+        // lookup the vertices
+        Vertex v1 = vertices.get(v1name);
+        Vertex v2 = vertices.get(v2name);
 
-   public Graph() {
-      vertices = new HashMap<>();
-   }
+        // add an adjacency of v2 to v1
+        v1.addAdjacency(v2);
 
-   public void addVertex(String name) {
-      vertices.put(name, new Vertex(name));
-   }
+        // add an adjacency of v1 to v2
+        v2.addAdjacency(v1);
+    }
 
-   public void addEdge(String v1name, String v2name) {
-      // collect the references to each vertex
-      Vertex v1 = vertices.get(v1name);
-      Vertex v2 = vertices.get(v2name);
+    public Vertex getVertex(String name) {
+        return vertices.get(name);
+    }
 
-      // add an adjacency from v1 to v2 AND an adjacency from v2 to v1
-      v1.addAdjacency(v2);
-      v2.addAdjacency(v1);
-   }
+    public static void main(String[] args) {
+        AdjacencyListGraph graph = new AdjacencyListGraph();
 
-   public Vertex getVertex(String name) {
-      return vertices.get(name);
-   }
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addVertex("D");
+        graph.addVertex("E");
 
-   // implement DFS
+        graph.addEdge("A", "B");
+        graph.addEdge("A", "C");
+        graph.addEdge("B", "D");
+        graph.addEdge("B", "E");
+        graph.addEdge("C", "D");
+        graph.addEdge("D", "E");
 
-   public static void main(String[] args) {
-      Graph graph = new Graph();
+        Vertex d = graph.getVertex("D");
+        System.out.println("Degree of D: " + d.getDegree());
 
-      graph.addVertex("A");
-      graph.addVertex("B");
-      graph.addVertex("C");
-      graph.addVertex("D");
-      graph.addVertex("E");
+        List<Vertex> adjacencies = d.getAdjacencies();
+        System.out.println("Adjacencies of D:");
+        for (Vertex v : adjacencies) {
+            System.out.println("\t" + v.getName());
 
-      graph.addEdge("A", "B");
-      graph.addEdge("A", "C");
-      graph.addEdge("B", "D");
-      graph.addEdge("C", "D");
-      graph.addEdge("B", "E");
-      graph.addEdge("D", "E");
-
-      // test DFS
-   }
+            System.out.print("\t" + v.getName() + "'s adjacencies:");
+            for (Vertex x: v.getAdjacencies()) {
+                System.out.print(x.getName() + " ");
+            }
+            System.out.println("");
+        }
+    }
 }
